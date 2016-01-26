d07db9ccd9475b0e92542fe17fe838e3ba5b3842
ProfitWarning <christian.fastner@arcor.de>
2016-01-26 15:15:58 +0100

    fix testing

--- src/test.js
+++
@@ -1,4 +0,0 @@
-var childProcess = require('child_process');
-var kill = require('tree-kill');
-var utils = require('./utils');
-var watch;
--- tasks/build_angular/unit-tests.js
+++ tasks/build_angular/unit-tests.js
@@ -7,23 +7,29 @@ var conf = require('./conf');
 var karma = require('karma');
 
 var pathSrcHtml = [
-  path.join(conf.paths.src, '/**/*.html')
+  path.join(conf.paths.src, '/app/**/*.html')
 ];
 
 var pathSrcJs = [
-  path.join(conf.paths.src, '/**/!(*.spec).js')
+  path.join(conf.paths.src, '/app/**/!(*.spec).js')
 ];
 
 function runTests (singleRun, done) {
   var reporters = ['progress'];
   var preprocessors = {};
 
+   console.log('sdasdasdsadsadsadsadas!!!!!!!!!!!!!!!!!');
+  
   pathSrcHtml.forEach(function(path) {
+	  console.log(path);
+	if(path.indexOf('node_modules') === -1)
     preprocessors[path] = ['ng-html2js'];
   });
 
   if (singleRun) {
     pathSrcJs.forEach(function(path) {
+		console.log(path);
+	  if(path.indexOf('node_modules') === -1)
       preprocessors[path] = ['coverage'];
     });
     reporters.push('coverage')
